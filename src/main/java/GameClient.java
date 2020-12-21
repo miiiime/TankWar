2012058ecb00e1c2552b5fc513a2bb26820c16c4
Eruku <jdn4354jdn@gmail.com>
2020-12-21 21:30:55 +0800

    造牆

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,32 +1,37 @@
 import object.Direction;
 import object.Tank;
+import object.Wall;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
-public class GameClient extends JComponent
-{
+public class GameClient extends JComponent {
     private int screenWide;
     private int screenHeight;
     private boolean stop;
 
     Tank playerTank;
+    private ArrayList<Tank> enemyTank = new ArrayList<>();
+    private ArrayList<Wall> walls = new ArrayList<>();
 
-    GameClient(){
-        this(800,600);
+
+    GameClient() {
+        this(800, 600);
     }
 
-    GameClient(int screenWide,int screenHeight){
-        this.screenWide=screenWide;
-        this.screenHeight=screenHeight;
-        this.setPreferredSize(new Dimension(screenWide,screenHeight));
-        playerTank=new Tank(100,100,Direction.RIGHT);
+    GameClient(int screenWide, int screenHeight) {
+        this.screenWide = screenWide;
+        this.screenHeight = screenHeight;
+        this.setPreferredSize(new Dimension(screenWide, screenHeight));
+
+        init();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for(;!stop;){
+                for (; !stop; ) {
                     repaint();
                     try {
                         Thread.sleep(50);
@@ -37,48 +42,67 @@ public class GameClient extends JComponent
             }
         }).start();
     }
+    //物件建構
+    public void init() {
+        playerTank = new Tank(50, 425, Direction.RIGHT);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                enemyTank.add(new Tank(600 + i * 50, 200 + j * 50, Direction.LEFT, true));
+            }
+        }
+        walls.add(new Wall(160,140,false,3));
+        walls.add(new Wall(160,396,false,3));
+        walls.add(new Wall(0,108,true,9));
+        walls.add(new Wall(0,492,true,9));
+        walls.add(new Wall(480,204,false,7));
+    }
+
 
     @Override
     protected void paintComponent(Graphics g) {
         g.setColor(Color.DARK_GRAY);
-        g.fillRect(0,0,screenWide,screenHeight);
+        g.fillRect(0, 0, screenWide, screenHeight);
         playerTank.draw(g);
+        for (Tank t : enemyTank) {
+            t.draw(g);
+        }
+        for (Wall w : walls) {
+            w.draw(g);
+        }
     }
 
-    public void keyPressed(KeyEvent k){
-        boolean[] dirs=playerTank.getDirs();
-        switch (k.getKeyCode())
-        {
+    public void keyPressed(KeyEvent k) {
+        boolean[] dirs = playerTank.getDirs();
+        switch (k.getKeyCode()) {
             case KeyEvent.VK_RIGHT:
-                dirs[0]=true;
+                dirs[0] = true;
                 break;
             case KeyEvent.VK_DOWN:
-                dirs[1]=true;
+                dirs[1] = true;
                 break;
             case KeyEvent.VK_LEFT:
-                dirs[2]=true;
+                dirs[2] = true;
                 break;
             case KeyEvent.VK_UP:
-                dirs[3]=true;
+                dirs[3] = true;
                 break;
         }
     }
 
-    public void keyReleased(KeyEvent k){
-        boolean[] dirs=playerTank.getDirs();
-        switch (k.getKeyCode())
-        {
+    public void keyReleased(KeyEvent k) {
+        boolean[] dirs = playerTank.getDirs();
+        switch (k.getKeyCode()) {
             case KeyEvent.VK_RIGHT:
-                dirs[0]=false;
+                dirs[0] = false;
                 break;
             case KeyEvent.VK_DOWN:
-                dirs[1]=false;
+                dirs[1] = false;
                 break;
             case KeyEvent.VK_LEFT:
-                dirs[2]=false;
+                dirs[2] = false;
                 break;
             case KeyEvent.VK_UP:
-                dirs[3]=false;
+                dirs[3] = false;
                 break;
         }
     }
--- src/main/java/object/Tank.java
+++ src/main/java/object/Tank.java
@@ -4,45 +4,78 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Tank {
-    private int x;
-    private int y;
+    private double x;
+    private double y;
     private int speed;
     private Direction direction;
-    private boolean[] dirs=new boolean[4];
+    private boolean[] dirs = new boolean[4];
+    private boolean enemy;
+    private int pmx;
+    private int pmy;
 
     public Tank(int x, int y, Direction direction) {
+        this(x, y, direction, false);
+    }
+
+    public Tank(int x, int y, Direction direction, boolean enemy) {
         this.x = x;
         this.y = y;
         this.direction = direction;
-        this.speed=8;
+        this.speed = 5;
+        this.enemy = enemy;
     }
 
-    public Image getImage(){
-        if(direction==Direction.RIGHT)
-            return new ImageIcon("assets/images/itankR.png").getImage();
-        if(direction==Direction.RIGHT_DOWN)
-            return new ImageIcon("assets/images/itankRD.png").getImage();
-        if(direction==Direction.DOWN)
-            return new ImageIcon("assets/images/itankD.png").getImage();
-        if(direction==Direction.LEFT_DOWN)
-            return new ImageIcon("assets/images/itankLD.png").getImage();
-        if(direction==Direction.LEFT)
-            return new ImageIcon("assets/images/itankL.png").getImage();
-        if(direction==Direction.LEFT_UP)
-            return new ImageIcon("assets/images/itankLU.png").getImage();
-        if(direction==Direction.UP)
-            return new ImageIcon("assets/images/itankU.png").getImage();
-        if(direction==Direction.RIGHT_UP)
-            return new ImageIcon("assets/images/itankRU.png").getImage();
+    public Image getImage() {
+        String name = enemy ? "etank" : "itank";
+        if (direction == Direction.RIGHT) {
+            pmx = 18;
+            pmy = 24;
+            return new ImageIcon("assets/images/" + name + "R.png").getImage();
+        }
+        if (direction == Direction.RIGHT_DOWN) {
+            pmx = 22;
+            pmy = 22;
+            return new ImageIcon("assets/images/" + name + "RD.png").getImage();
+        }
+        if (direction == Direction.DOWN) {
+            pmx = 24;
+            pmy = 18;
+            return new ImageIcon("assets/images/" + name + "D.png").getImage();
+        }
+        if (direction == Direction.LEFT_DOWN) {
+            pmx = 26;
+            pmy = 22;
+            return new ImageIcon("assets/images/" + name + "LD.png").getImage();
+        }
+        if (direction == Direction.LEFT) {
+            pmx = 28;
+            pmy = 24;
+            return new ImageIcon("assets/images/" + name + "L.png").getImage();
+        }
+        if (direction == Direction.LEFT_UP) {
+            pmx = 26;
+            pmy = 26;
+            return new ImageIcon("assets/images/" + name + "LU.png").getImage();
+        }
+        if (direction == Direction.UP) {
+            pmx = 24;
+            pmy = 28;
+            return new ImageIcon("assets/images/" + name + "U.png").getImage();
+        }
+        if (direction == Direction.RIGHT_UP) {
+            pmx = 22;
+            pmy = 26;
+            return new ImageIcon("assets/images/" + name + "RU.png").getImage();
+        }
 
         return null;
     }
 
-    public int getX() {
+    public double getX() {
         return x;
     }
 
-    public int getY() {
+    public double getY() {
         return y;
     }
 
@@ -66,63 +99,65 @@ public class Tank {
         this.direction = direction;
     }
 
-    public boolean isStop(){
-        for(boolean b:dirs) {
+    public boolean isStop() {
+        for (boolean b : dirs) {
             if (b)
                 return false;
         }
         return true;
     }
 
-    public boolean determineDirection(){
-        Direction base=direction;
-        if(dirs[0]&&!dirs[1]&&!dirs[2]&&!dirs[3]) direction=Direction.RIGHT;
-        else if(dirs[0]&&dirs[1]&&!dirs[2]&&!dirs[3]) direction=Direction.RIGHT_DOWN;
-        else if(!dirs[0]&&dirs[1]&&!dirs[2]&&!dirs[3]) direction=Direction.DOWN;
-        else if(!dirs[0]&&dirs[1]&&dirs[2]&&!dirs[3]) direction=Direction.LEFT_DOWN;
-        else if(!dirs[0]&&!dirs[1]&&dirs[2]&&!dirs[3]) direction=Direction.LEFT;
-        else if(!dirs[0]&&!dirs[1]&&dirs[2]&&dirs[3]) direction=Direction.LEFT_UP;
-        else if(!dirs[0]&&!dirs[1]&&!dirs[2]&&dirs[3]) direction=Direction.UP;
-        else if(dirs[0]&&!dirs[1]&&!dirs[2]&&dirs[3]) direction=Direction.RIGHT_UP;
-        return base==direction;
+    public boolean determineDirection() {
+        Direction base = direction;
+        if (dirs[0] && !dirs[1] && !dirs[2] && !dirs[3]) direction = Direction.RIGHT;
+        else if (dirs[0] && dirs[1] && !dirs[2] && !dirs[3]) direction = Direction.RIGHT_DOWN;
+        else if (!dirs[0] && dirs[1] && !dirs[2] && !dirs[3]) direction = Direction.DOWN;
+        else if (!dirs[0] && dirs[1] && dirs[2] && !dirs[3]) direction = Direction.LEFT_DOWN;
+        else if (!dirs[0] && !dirs[1] && dirs[2] && !dirs[3]) direction = Direction.LEFT;
+        else if (!dirs[0] && !dirs[1] && dirs[2] && dirs[3]) direction = Direction.LEFT_UP;
+        else if (!dirs[0] && !dirs[1] && !dirs[2] && dirs[3]) direction = Direction.UP;
+        else if (dirs[0] && !dirs[1] && !dirs[2] && dirs[3]) direction = Direction.RIGHT_UP;
+        return base == direction;
     }
 
-    public void move(){
-        switch (direction)
-        {
+    public void move() {
+        switch (direction) {
             case RIGHT:
-                x+=speed;
+                x += speed;
                 break;
             case RIGHT_DOWN:
-                x+=speed/2;
-                y+=speed/2;
+                x += speed / 2;
+                y += speed / 2;
                 break;
             case DOWN:
-                y+=speed;
+                y += speed;
                 break;
             case LEFT_DOWN:
-                x-=speed/2;
-                y+=speed/2;
+                x -= speed / 2;
+                y += speed / 2;
                 break;
             case LEFT:
-                x-=speed;
+                x -= speed;
                 break;
             case LEFT_UP:
-                y-=speed/2;
-                x-=speed/2;
+                y -= speed / 2;
+                x -= speed / 2;
                 break;
             case UP:
-                y-=speed;
+                y -= speed;
                 break;
             case RIGHT_UP:
-                y-=speed/2;
-                x+=speed/2;
+                y -= speed / 2;
+                x += speed / 2;
                 break;
         }
     }
 
-    public void draw(Graphics g){
-        if(!isStop()&&determineDirection())move();
-        g.drawImage(getImage(),x,y,null);
+    public void draw(Graphics g) {
+        if (!isStop()) {
+            determineDirection();
+            move();
+        }
+        g.drawImage(getImage(), (int) (x - pmx), (int) (y - pmy), null);
     }
 }
